a2d58816ae13714f64be848901e70896ab77708b
saulojustiniano1 <saulojustiniano1@gmail.com>
2023-04-19 14:07:24 -0300

    📦create: Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Logo from './objects/Logo/index';
+import Menu from './objects/Menu/index';
 import Sobre from './objects/Sobre/index';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Logo />
       <Sobre />
+      <Menu />
     </>
   );
 }
---
+++ src/objects/Menu/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const Menu = () => {
+  return (
+    <a href="#menu" className="menu">
+      <span className="center">Menu</span>
+    </a>
+  );
+};
+
+export default Menu;
