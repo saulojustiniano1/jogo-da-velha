380df34140776e716db4a0ac52bb487d481d4241
saulojustiniano1 <saulojustiniano1@gmail.com>
2023-04-19 13:38:45 -0300

    📦create: importando o logo da CollabCode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Logo from './objects/Logo/index';
 import Sobre from './objects/Sobre/index';
 
 function App() {
-  return <Sobre />;
+  return (
+    <div>
+      <Logo />
+      <Sobre />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/objects/Logo/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CollabCode from '../../assets/logo.png';
+
+const Logo = () => {
+  return <img src={CollabCode} alt="Logo da CollabCode" />;
+};
+
+export default Logo;
