152747fe5699ee3d94e42c90a2e25fbb2bce9d46
saulojustiniano1 <saulojustiniano1@gmail.com>
2023-04-24 16:24:54 -0300

    📤update: refatorando o codido

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css';
 import Hashtag from './components/Hashtag';
 import Header from './components/Header/index';
 
-const App = () => (
-  <main className="app">
-    <Header />
-    <Hashtag />
-  </main>
-);
+const App = () => {
+  return (
+    <main className="app">
+      <Header />
+      <Hashtag />
+    </main>
+  );
+};
 
 export default App;
