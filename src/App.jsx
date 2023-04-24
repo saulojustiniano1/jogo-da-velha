7ed228914b6ebe57f4b37a367835c4634c4e5a9a
saulojustiniano1 <saulojustiniano1@gmail.com>
2023-04-24 15:45:38 -0300

    📤update: posicionar o nosso tabuleiro e refatorando o codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import './App.css';
 import Hashtag from './components/Hashtag';
 import Header from './components/Header/index';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Hashtag />
-    </>
-  );
-}
+const App = () => (
+  <main className="app">
+    <Header />
+    <Hashtag />
+  </main>
+);
 
 export default App;
