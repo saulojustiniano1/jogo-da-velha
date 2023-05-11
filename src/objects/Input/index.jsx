e9b2b3f23cd7acf596808017d2841a14c8ea5c53
saulojustiniano1 <saulojustiniano1@gmail.com>
2023-05-11 16:40:43 -0300

    📦create: Comportamento do Checkbox selecionado

--- src/objects/Input/index.jsx
+++ src/objects/Input/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Input = ({ id = '', value = '', type = 'text' }) => {
-  return <input id={id} type={type} value={value} />;
+  return <input className="input" id={id} type={type} value={value} />;
 };
 
 export default Input;
