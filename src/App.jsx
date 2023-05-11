d43071dba2e64ba21e938f7618cf2e1f7411a965
saulojustiniano1 <saulojustiniano1@gmail.com>
2023-05-11 14:04:00 -0300

    📦create: object Input

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 
 import Hashtag from './components/Hashtag';
 import Header from './components/Header/index';
+import Input from './objects/Input';
 import Label from './objects/Label';
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     <main className="app">
       <Header />
       <Hashtag />
+      <Input value="show" />
       <Label content="Mostrar eventos" />
     </main>
   );
